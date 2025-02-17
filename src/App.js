4c13d71d4ad7abfb5f0585a72563f63372518639
murodil01 <nurmamatovmirodil978@gmail.com>
2025-02-17 15:31:09 +0500

    barbir

---
+++ src/App.js
@@ -0,0 +1,49 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="apple container">
+        <header className="navbar">
+            <div className="logo">
+                <a className="logo-link">APPSTORE.UZ <br/> Uzbekistan</a>
+            </div>
+            <nav>
+                <ul className="nav-links">
+                    <li><a href="!">Home</a></li>
+                    <li><a href="!">Gallery</a></li>
+                    <li><a href="!">News</a></li>
+                    <li><a href="!">About Us</a></li>
+                    <a href="!"><i className='bx bx-cart' style={{ color: "#ffffff" }}></i></a>
+                </ul>
+            </nav>
+        </header>
+
+        <main className="hero-section">
+            <div className="hero">
+                <div className="text-content">
+                    <h1>iPhone 12 Pro</h1>
+                    <p>The iPhone 12 Pro and iPhone 12 Pro Max are part of Apple's latest generation of smartphones,
+                        offering 5G connectivity, the A14 chip for better performance.</p>
+                    <div className="buttons">
+                        <button className="btn order-now">Order now</button>
+                        <button className="btn learn-more">Learn more</button>
+                    </div>
+
+                    <footer className="pagination">
+                        <span className="span">01</span>
+                        <span className="span">02</span>
+                        <span className="span">03</span>
+                        <span className="span">04</span>
+                        <span className="show"><i className='bx bx-chevron-left' style={{ color: "#ffffff" }}></i></span>
+                        <span className="show"><i className='bx bx-chevron-right' style={{ color: "#ffffff" }}></i></span>
+                    </footer>
+                </div>
+            </div>
+        </main>
+    </div>
+  );
+}
+
+export default App;
+
